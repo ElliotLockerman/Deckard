a45f8484aa5a80be9c18734ee88591c200436d1a
Elliot Lockerman <elliot@lockerman.info>
2024-05-31 08:54:29 -0400

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,9 @@ mod helpers;
 use helpers::*;
 
 
+#[derive(PartialEq, Eq)]
 enum Phase {
-    Setup,
+    Startup,
     Running,
     Output,
 }
@@ -42,7 +43,7 @@ struct App {
     modal: Option<ModalContents>,
 }
 
-fn starting_root() -> PathBuf {
+fn default_root() -> PathBuf {
     match homedir::get_my_home() {
         Ok(path_opt) => path_opt.unwrap_or(PathBuf::from("/")),
         Err(_) => PathBuf::from("/"),
@@ -52,8 +53,8 @@ fn starting_root() -> PathBuf {
 impl App {
     fn new() -> App {
         App {
-            phase: Phase::Setup,
-            root: starting_root(),
+            phase: Phase::Startup,
+            root: default_root(),
             thread: None,
             images: None,
             errors: vec![],
@@ -61,13 +62,9 @@ impl App {
         }
     }
 
-    fn handle_modal(&mut self, ctx: &egui::Context) {
-        if let Some(contents) = &self.modal {
-            if draw_error_modal(ctx, &contents) {
-                self.modal = None;
-            }
-        }
-    }
+////////////////////////////////////////////////////////////////////////////////
+// Phase Core Functions
+////////////////////////////////////////////////////////////////////////////////
 
     fn phase_startup(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
         assert!(self.thread.is_none());
@@ -81,21 +78,16 @@ impl App {
         }
 
         if ui.button("Search").clicked() {
-            self.errors.clear();
-            self.phase = Phase::Running;
-            let root = self.root.clone();
-            self.thread = Some(thread::spawn(move ||
-                search::search(root, false, None, None)
-            ));
+            self.start_running();
         }
-
     }
 
     fn phase_running(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
         if self.thread.is_some() {
             let done = self.thread.as_ref().unwrap().is_finished();
             if done {
-                self.load_images();
+                let images = self.load_images();
+                self.start_output(images);
                 return;
             }
         } else {
@@ -104,10 +96,128 @@ impl App {
 
         ui.label(format!("Running on {}...", self.root.display()));
         ui.spinner();
-
     }
 
-    fn load_images(&mut self) {
+    fn phase_output(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
+        if ui.button("<- New Search").clicked() {
+            self.start_startup();
+            return;
+        }
+
+        ui.separator();
+
+        if let Some(images) = self.images.as_ref() {
+            if !images.is_empty() {
+                self.draw_output_table(ui);
+            } else {
+                ui.label(format!("Done on {}, found no duplicates", self.root.display()));
+            }
+        } else {
+            ui.label(format!("Error, no results found"));
+        }
+    }
+
+////////////////////////////////////////////////////////////////////////////////
+// Phase Transitions
+////////////////////////////////////////////////////////////////////////////////
+
+    fn start_startup(&mut self) {
+        assert!(self.thread.is_none());
+        self.errors.clear();
+        self.images = None;
+        self.phase = Phase::Startup;
+    }
+
+    fn start_running(&mut self) {
+        assert!(self.phase == Phase::Startup);
+        self.phase = Phase::Running;
+        let root = self.root.clone();
+        self.thread = Some(thread::spawn(move ||
+            search::search(root, false, None, None)
+        ));
+    }
+
+    fn start_output(&mut self, images: Vec<Vec<Image>>) {
+        assert!(self.phase == Phase::Running);
+        assert!(self.thread.is_none());
+        self.images = Some(images);
+        self.phase = Phase::Output;
+    }
+
+////////////////////////////////////////////////////////////////////////////////
+// Drawing Helpers
+////////////////////////////////////////////////////////////////////////////////
+
+    fn draw_output_table(&mut self,  ui: &mut egui::Ui) {
+        assert!(self.images.is_some());
+        egui::ScrollArea::both().show(ui, |ui| {
+            for (dup_idx, dups) in self.images.as_ref().unwrap().iter().enumerate() {
+                ui.push_id(dup_idx, |ui| {
+                    TableBuilder::new(ui)
+                        .column(Column::remainder().resizable(true))
+                        .column(Column::auto().resizable(true))
+                        .vscroll(false)
+                        .striped(true)
+                        .body(|body| {
+                            body.rows(100.0, dups.len(), |mut row| {
+                                let idx = row.index(); 
+                                let image = &dups[idx];
+                                row.col(|ui| {
+                                    ui.heading(format!("{}", image.path.display()));
+                                    if let Some((width, height)) = image.dimm {
+                                        ui.label(format!("{width}×{height}"));
+                                    }
+                                    ui.label(format_size(image.file_size, DECIMAL));
+                                    ui.horizontal(|ui| {
+                                        let err = if ui.button("Open").clicked() {
+                                            open_file(image.path.as_path(), OpenKind::Open)
+                                        } else if ui.button("Show").clicked() {
+                                            open_file(image.path.as_path(), OpenKind::Reveal)
+                                        } else {
+                                            Ok(())
+                                        };
+
+                                        if let Err(msg) = err {
+                                            self.modal = Some(ModalContents::new(
+                                                "Error showing file".to_string(),
+                                                msg,
+                                            ));
+                                        }
+                                    });
+                                });
+                                row.col(|ui| {
+                                    ui.add(egui::Image::from_bytes(
+                                            image.handle.clone(),
+                                            image.buffer.clone()
+                                        )
+                                    );
+                                });
+                            });
+                        });
+                });
+                ui.separator();
+            }
+
+            if !self.errors.is_empty() {
+                ui.heading(egui::RichText::new("Errors").color(egui::Color32::RED));
+                for err in &self.errors {
+                    ui.label(err);
+                }
+            }
+        });
+    }
+
+    fn draw_modal(&mut self, ctx: &egui::Context) {
+        if let Some(contents) = &self.modal {
+            if draw_error_modal(ctx, &contents) {
+                self.modal = None;
+            }
+        }
+    }
+
+////////////////////////////////////////////////////////////////////////////////
+
+    fn load_images(&mut self) -> Vec<Vec<Image>> {
         let thread = self.thread.take().unwrap();
         assert!(thread.is_finished());
         let results = thread.join().unwrap();
@@ -152,86 +262,8 @@ impl App {
             }
             images.push(vec);
         }
-        self.images = Some(images);
-        self.phase = Phase::Output;
+        images
     }
-
-    fn phase_output(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui) {
-        if ui.button("<- New Search").clicked() {
-            self.images = None;
-            self.phase = Phase::Setup;
-            return;
-        }
-
-        ui.separator();
-
-        if let Some(images) = self.images.as_ref() {
-            if images.len() > 0 {
-                self.draw_results_table(ui);
-            } else {
-                ui.label(format!("Done on {}, found no duplicates", self.root.display()));
-            }
-        } else {
-            ui.label(format!("Error, no results found"));
-        }
-    }
-
-    // Returns clicked (dup_idx, row)
-    fn draw_results_table(&mut self,  ui: &mut egui::Ui) {
-        assert!(self.images.is_some());
-        egui::ScrollArea::both().show(ui, |ui| {
-            for (dup_idx, dups) in self.images.as_ref().unwrap().iter().enumerate() {
-                ui.push_id(dup_idx, |ui| {
-                    TableBuilder::new(ui)
-                        .column(Column::remainder().resizable(true))
-                        .column(Column::auto().resizable(true))
-                        .vscroll(false)
-                        .striped(true)
-                        .body(|body| {
-                            body.rows(100.0, dups.len(), |mut row| {
-                                let idx = row.index(); 
-                                let image = &dups[idx];
-                                row.col(|ui| {
-                                    ui.heading(format!("{}", image.path.display()));
-                                    if let Some((width, height)) = image.dimm {
-                                        ui.label(format!("{width}×{height}"));
-                                    }
-                                    ui.label(format_size(image.file_size, DECIMAL));
-                                    ui.horizontal(|ui| {
-                                        let err = if ui.button("Open").clicked() {
-                                            open_file(image.path.as_path(), OpenKind::Open)
-                                        } else if ui.button("Show").clicked() {
-                                            open_file(image.path.as_path(), OpenKind::Reveal)
-                                        } else {
-                                            Ok(())
-                                        };
-
-                                        if let Err(msg) = err {
-                                            self.modal = Some(ModalContents::new(
-                                                "Error showing file".to_string(),
-                                                msg,
-                                            ));
-                                        }
-                                    });
-                                });
-                                row.col(|ui| {
-                                    ui.add(egui::Image::from_bytes(image.handle.clone(), image.buffer.clone()));
-                                });
-                            });
-                        });
-                });
-                ui.separator();
-            }
-
-            if self.errors.len() > 0 {
-                ui.heading(egui::RichText::new("Errors").color(egui::Color32::RED));
-                for err in &self.errors {
-                    ui.label(err);
-                }
-            }
-        });
-    }
-
 }
 
 
@@ -240,12 +272,12 @@ impl eframe::App for App {
         egui_extras::install_image_loaders(ctx);
         egui::CentralPanel::default().show(ctx, |ui| {
             match self.phase {
-                Phase::Setup => self.phase_startup(ctx, ui),
+                Phase::Startup => self.phase_startup(ctx, ui),
                 Phase::Running => self.phase_running(ctx, ui),
                 Phase::Output => self.phase_output(ctx, ui),
             }
         });
-        self.handle_modal(ctx);
+        self.draw_modal(ctx);
     }
 }
 
